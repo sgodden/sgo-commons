3bf620c1f57a8c299d4c7dc7bd882957efc234af
sgodden@gmail.com <sgodden@gmail.com@0006e999-4a4a-0410-b729-572a85ca9b06>
2008-11-03 12:24:33 +0000

    Changes to allow the query service implementation to receive a guice provider of sessions rather than a session factory.  In a clustered environment, it can be problematic to hold on to references to a session factory.

--- src/main/java/org/sgodden/query/service/QueryService.java
+++ src/main/java/org/sgodden/query/service/QueryService.java
@@ -16,8 +16,11 @@
 # ================================================================= */
 package org.sgodden.query.service;
 
+import org.hibernate.Session;
 import org.sgodden.query.Query;
 import org.sgodden.query.ResultSet;
+
+import com.google.inject.Provider;
 
 /**
  * A service which runs queries and returns results.
